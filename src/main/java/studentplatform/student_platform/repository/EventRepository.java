34c19ecd8c42f037df27090b6639cc3319d7bdf0
unknown <roseprim016@gmail.com>
2025-08-27 12:34:08 +0630

    Testing git push

--- src/main/java/studentplatform/student_platform/repository/EventRepository.java
+++ src/main/java/studentplatform/student_platform/repository/EventRepository.java
@@ -1,5 +1,5 @@
 package studentplatform.student_platform.repository;
-
+//Htet Htet 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
